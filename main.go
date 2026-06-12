f7064ad91b6fdc921952458614ee0a15543264af
Matt Box <mdbox037a@protonmail.com>
2026-06-11 21:57:43 -0400

    set up main to read config and set username

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+
+	"github.com/mdbox037a/gator/internal/config"
+)
+
+const currentUser string = "matt"
+
+func main() {
+	currentConfig, err := config.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = currentConfig.SetUser(currentUser)
+	currentConfig, err = config.Read()
+	fmt.Printf("%+v\n", currentConfig)
+}
