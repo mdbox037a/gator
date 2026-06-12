5a4e0ed6a15dde24bccdd9ebedcf40c7c1f7eea5
Matt Box <mdbox037a@protonmail.com>
2026-06-12 06:24:32 -0400

    add error handling to read and setuser ops in main()

--- main.go
+++ main.go
@@ -12,10 +12,17 @@ const currentUser string = "matt"
 func main() {
 	currentConfig, err := config.Read()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error: failed to read config - %v", err)
 	}
 
 	err = currentConfig.SetUser(currentUser)
+	if err != nil {
+		log.Fatalf("Error: failed to set user - %v\n", err)
+	}
+
 	currentConfig, err = config.Read()
+	if err != nil {
+		log.Fatalf("Error: failed to read config - %v", err)
+	}
 	fmt.Printf("%+v\n", currentConfig)
 }
