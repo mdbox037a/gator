1fdbe8ae1e31022259b813edbcac6c5ccf21234b
Matt Box <mdbox037a@protonmail.com>
2026-06-15 07:29:32 -0400

    create handlerRegister() to add new users to db

--- handler_user.go
+++ handler_user.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/mdbox037a/gator/internal/database"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -16,5 +22,33 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	fmt.Println("Info: username set successfully")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) == 0 {
+		return errors.New("Error: please provide a username")
+	}
+
+	ctx := context.Background()
+	userArgs := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      cmd.Args[0],
+	}
+	user, err := s.db.CreateUser(ctx, userArgs)
+	if err != nil {
+		return fmt.Errorf("Error: failed to create user %s - %v", userArgs.Name, err)
+	}
+	s.currentConfig.CurrentUserName = user.Name
+
+	debugUserInfo, err := json.MarshalIndent(user, "", "    ")
+	if err != nil {
+		fmt.Printf("Warning: failed to marshal user info for debugging")
+	}
+	fmt.Printf("Info: user %s successfully created")
+	fmt.Printf("Debug: new user info: \n%s\n", string(debugUserInfo))
+
 	return nil
 }
