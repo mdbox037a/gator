6183c731ac193ab74cd1a51455c3d044d4b4bda7
Matt Box <mdbox037a@protonmail.com>
2026-06-18 21:05:02 -0400

    create query to list all followed feeds for user; regenerate sqlc code

--- internal/database/feed_follows.sql.go
+++ internal/database/feed_follows.sql.go
@@ -71,3 +71,55 @@ func (q *Queries) CreateFeedFollow(ctx context.Context, arg CreateFeedFollowPara
 	)
 	return i, err
 }
+
+const getFeedFollowForUser = `-- name: GetFeedFollowForUser :many
+SELECT
+  feed_follows.id, feed_follows.created_at, feed_follows.updated_at, feed_follows.user_id, feed_follows.feed_id,
+  feeds.name AS feed_name,
+  users.name AS following_user
+FROM feed_follows
+INNER JOIN users ON feed_follows.user_id = users.id
+INNER JOIN feeds ON feed_follows.feed_id = feed.id
+WHERE $1 = users.name
+`
+
+type GetFeedFollowForUserRow struct {
+	ID            uuid.UUID
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	UserID        uuid.UUID
+	FeedID        uuid.UUID
+	FeedName      string
+	FollowingUser string
+}
+
+func (q *Queries) GetFeedFollowForUser(ctx context.Context, name string) ([]GetFeedFollowForUserRow, error) {
+	rows, err := q.db.QueryContext(ctx, getFeedFollowForUser, name)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []GetFeedFollowForUserRow
+	for rows.Next() {
+		var i GetFeedFollowForUserRow
+		if err := rows.Scan(
+			&i.ID,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+			&i.UserID,
+			&i.FeedID,
+			&i.FeedName,
+			&i.FollowingUser,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -17,3 +17,14 @@ SELECT
 FROM inserted_feed_follow
 INNER JOIN users ON inserted_feed_follow.user_id = users.id
 INNER JOIN feeds ON inserted_feed_follow.feed_id = feeds.id;
+
+
+-- name: GetFeedFollowForUser :many
+SELECT
+  feed_follows.*,
+  feeds.name AS feed_name,
+  users.name AS following_user
+FROM feed_follows
+INNER JOIN users ON feed_follows.user_id = users.id
+INNER JOIN feeds ON feed_follows.feed_id = feed.id
+WHERE $1 = users.name;
