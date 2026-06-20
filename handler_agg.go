253bfd3e29cc6e4f873197fa4859755e9fcbe15d
Matt Box <mdbox037a@protonmail.com>
2026-06-20 07:27:26 -0400

    complete scrapeFeeds function

--- handler_agg.go
+++ handler_agg.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"encoding/xml"
 	"fmt"
+	"time"
+
+	"github.com/mdbox037a/gator/internal/database"
 )
 
 const feedURL string = "https://www.wagslane.dev/index.xml"
@@ -19,5 +22,30 @@ func handlerAgg(s *state, cmd command) error {
 		fmt.Errorf("Error: failed to marshal RSSFeed contents to print - %v", err)
 	}
 	fmt.Printf("Debug: RSSFeed contents:\n%s\n", string(feedContents))
+	return nil
+}
+
+func scrapeFeeds(s *state) error {
+	ctx := context.Background()
+	feed, err := s.db.GetNextFeedToFetch(ctx)
+	if err != nil {
+		return fmt.Errorf("Error: failed to get next feed to fetch from database - %v", err)
+	}
+
+	err = s.db.MarkFeedFetched(ctx, feed.ID)
+	if err != nil {
+		return fmt.Errorf("Error: failed to mark feed '%s' as fetched - %n", feed.Name, err)
+	}
+
+	feedContent, err := fetchFeed(ctx, feed.Url)
+	if err != nil {
+		return fmt.Errorf("Error: failed to retrieve RSS feed from %s - %v", feed.Url, err)
+	}
+
+	feedPrintout, err := xml.MarshalIndent(feedContent, "", "    ")
+	if err != nil {
+		fmt.Errorf("Error: failed to marshal RSSFeed contents to print - %v", err)
+	}
+	fmt.Printf("Debug: RSSFeed contents:\n%s\n", string(feedPrintout))
 	return nil
 }
