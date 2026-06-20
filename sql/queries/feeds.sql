84effeb4796e892a0b19e752099726541a2b69b5
Matt Box <mdbox037a@protonmail.com>
2026-06-20 07:04:50 -0400

    create new query to get next feed to check; regen sqlc code

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -31,3 +31,10 @@ SET
   last_fetched_at = $2,
   updated_at = $2
 WHERE id = $1;
+
+
+-- name: GetNextFeedToFetch :one
+SELECT *
+FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST, id ASC
+LIMIT 1;
