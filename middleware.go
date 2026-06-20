75b367609e4d4f17c6a0cfbeb490c6ec99fc87bf
Matt Box <mdbox037a@protonmail.com>
2026-06-19 21:15:05 -0400

    add logic to get user struct, handle errors, and call handler

--- middleware.go
+++ middleware.go
@@ -1,7 +1,24 @@
 package main
 
-import ()
+import (
+	"context"
+	"fmt"
+
+	"github.com/mdbox037a/gator/internal/database"
+)
 
 func middlewareLoggedIn(handler func(s *state, cmd command, user database.User) error) func(*state, command) error {
+	return func(s *state, cmd command) error {
+		ctx := context.Background()
+		user, err := s.db.GetUser(ctx, s.currentConfig.CurrentUserName)
+		if err != nil {
+			return fmt.Errorf("Error: failed to retrieve user info for %s - %v", s.currentConfig.CurrentUserName, err)
+		}
 
+		err = handler(s, cmd, user)
+		if err != nil {
+			return fmt.Errorf("Error: call to handler function failed - %v", err)
+		}
+		return nil
+	}
 }
